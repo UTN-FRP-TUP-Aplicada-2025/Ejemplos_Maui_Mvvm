4194d9b66403511650ec6021a27f05a7223c6288
fernandofilipuzzi-utn <fernandofilipuzzi.utn@gmail.com>
2026-02-14 22:33:46 -0300

    agregando estilo en shell - sin esto crash al inicio

--- Ejemplos_Maui_Mvvm/Ejemplo/Pages/DialogPage.xaml.cs
+++
@@ -1,9 +0,0 @@
-namespace Ejemplo.Pages;
-
-public partial class DialogPage : ContentPage
-{
-	public DialogPage()
-	{
-		InitializeComponent();
-	}
-}
---
+++ Grafica/comandos.bat
@@ -0,0 +1,3 @@
+SET PATH=C:\Program Files\Inkscape\bin;%PATH%
+inkscape icono_base_1024.png --export-filename=appicon.svg
+inkscape icono_base_1024.png --export-filename=appiconconfg.svg
