ca60665e65470ed19d2c291d3dfc55c277d3f208
fernandofilipuzzi-utn <fernandofilipuzzi.utn@gmail.com>
2026-02-14 21:36:29 -0300

    diseño de pagina de login en modelo proyecto de modelo de login

--- Ejemplos_Maui_Mvvm/Ejemplo_Login/PageModels/LoginPageModel.cs
+++ Ejemplos_Maui_Mvvm/Ejemplo_Login/PageModels/LoginPageModel.cs
@@ -1,10 +1,18 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using CommunityToolkit.Maui.Alerts;
+using CommunityToolkit.Maui.Core;
+using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using Ejemplo_Login.Pages;
 namespace Ejemplo_Login.PageModels;
 
 public partial class LoginPageModel : ObservableObject
 {
+    [ObservableProperty]
+    private string usuario;
+
+    [ObservableProperty]
+    private string clave;
+
     public LoginPageModel( )
     {
     }
@@ -12,6 +20,23 @@ public partial class LoginPageModel : ObservableObject
     [RelayCommand]
     async private Task Login()
     {
+        if (string.IsNullOrEmpty(Usuario))
+        {
+            await Toast.Make("Complete el campo usuario", ToastDuration.Long).Show();
+            return;
+        }
+
+        if (string.IsNullOrEmpty(Clave))
+        {
+            await Toast.Make("Complete el campo clave", ToastDuration.Long).Show();
+            return;
+        }
+
+        if (Usuario != "admin" || clave != "1234")
+        {
+            await Toast.Make("Usuario o Clave incorrecto", ToastDuration.Long).Show();
+            return;
+        }
         await Shell.Current.GoToAsync($"//{nameof(MainPage)}");
     }
 }
